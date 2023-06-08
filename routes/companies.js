301ebdbeea1a6f850a4164f9e0bcaa741fda73ca
Kle012 <kimhle.0102@gmail.com>
2023-06-07 17:35:20 -0700

    Filter on provided search: name, min and max employess (optional)

--- routes/companies.js
+++ routes/companies.js
@@ -5,12 +5,13 @@
 const jsonschema = require("jsonschema");
 const express = require("express");
 
-const { BadRequestError } = require("../expressError");
+const { BadRequestError, ExpressError } = require("../expressError");
 const { ensureLoggedIn } = require("../middleware/auth");
 const Company = require("../models/company");
 
 const companyNewSchema = require("../schemas/companyNew.json");
 const companyUpdateSchema = require("../schemas/companyUpdate.json");
+const companySearchSchema = require("../schemas/companySearch.json");
 
 const router = new express.Router();
 
@@ -51,8 +52,20 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
  */
 
 router.get("/", async function (req, res, next) {
+  let q = req.query;
+
+  // Convert from querystring to ints
+  if (q.minEmployees !== undefined) q.minEmployees = +q.minEmployees;
+  if (q.maxEmployees !== undefined) q.maxEmployees = +q.maxEmployees;
+
   try {
-    const companies = await Company.findAll();
+    const result = jsonschema.validate(q, companySearchSchema);
+    if (!result.valid) {
+      let listErrors = result.errors.map(error => error.stack);
+      throw new BadRequestError(listErrors);
+    }
+
+    const companies = await Company.findAll(q);
     return res.json({ companies });
   } catch (err) {
     return next(err);
