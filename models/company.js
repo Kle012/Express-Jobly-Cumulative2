5c003975a35a307f57be80bc0157f413e074dd7b
Kle012 <kimhle.0102@gmail.com>
2023-06-08 18:10:53 -0700

    Add jobs to each company, if any

--- models/company.js
+++ models/company.js
@@ -119,9 +119,21 @@ class Company {
            WHERE handle = $1`,
         [handle]);
 
-    const company = companyRes.rows[0];
+    let company = companyRes.rows[0];
 
     if (!company) throw new NotFoundError(`No company: ${handle}`);
+
+    const jobRes = await db.query(
+        `SELECT id,
+                title,
+                salary,
+                equity
+        FROM jobs
+        WHERE company_handle = $1
+        ORDER BY id,`
+        [handle]);
+
+    company.jobs = jobRes.rows;
 
     return company;
   }
